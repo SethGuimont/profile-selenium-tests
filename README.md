72d1454ee3ffe62a9c972311fbba3e1416f91ec4
Seth Guimont <sethguimont@outlook.com>
2023-08-11 16:01:27 -0500

    Added more tests

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Just testing how Selenium tests work in Rust.
+Just testing how Selenium tests work in Rust.  Will break all 
+tests out into their own functions but for now getting a feel 
+on how it works.
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,41 @@ use thirtyfour::prelude::*;
 
 #[tokio::main]
 async fn main() -> WebDriverResult<()> {
-    let caps = DesiredCapabilities::chrome();
+    let mut caps = DesiredCapabilities::chrome();
+    caps.add_chrome_arg("--enable-automation")?;
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
 
     // Navigate to my profile page
     driver.goto("https://sethguimont.github.io").await?;
     assert_eq!(driver.title().await?, "Seth's Portfolio Page - Home");
+    println!("Title = {}", driver.title().await?);
+
+    driver.find(By::Tag("header-component")).await?;
+    driver.find(By::Tag("footer-component")).await?;
+
+    // Navigate to projects page
+    driver.goto("https://sethguimont.github.io/projects.html").await?;
+    println!("Title = {}", driver.title().await?);
+    driver.find(By::Tag("header-component")).await?;
+    driver.find(By::Tag("footer-component")).await?;
+
+    // Navigate to Blog
+    driver.goto("https://sethguimont.github.io/blog.html").await?;
+    println!("Title = {}", driver.title().await?);
+    driver.find(By::Tag("header-component")).await?;
+    driver.find(By::Tag("footer-component")).await?;
+
+    //Navigate to Contact
+    driver.goto("https://sethguimont.github.io/contact.html").await?;
+    println!("Title = {}", driver.title().await?);
+    driver.find(By::Tag("header-component")).await?;
+    driver.find(By::Tag("footer-component")).await?;
+
+    // Navigate to Discord
+    driver.goto("https://sethguimont.github.io/discord.html").await?;
+    println!("Title = {}", driver.title().await?);
+    driver.find(By::Tag("header-component")).await?;
+    driver.find(By::Tag("footer-component")).await?;
 
     // Always explicitly close the browser.
     driver.quit().await?;
